da2276726ff2d422700b3f9dc55f5f10d82973ca
RohitajDeshmukh <rohitrajdeshmukh9960@gmail.com>
2021-09-30 09:49:35 +0530

    update feat

--- app.js
+++ app.js
@@ -9,20 +9,22 @@ function calculateProfitLoss(initial, quantity, current){
     if(initial>current){
         const loss = (initial-current)*quantity;
         const lossPercentage = (loss/initial) * 100;
-        console.log(`loss is ${loss} and percentage of loss is ${lossPercentage}`)
+        output.innerHTML = `loss is ${loss} and percentage of loss is ${lossPercentage}`;
     }else if(current>initial){
         const profit = (current-initial)*quantity;
         const profitPercentage = (profit/initial) * 100;
-        console.log(`profit is ${profit} and percentage of profit is ${profitPercentage}`)
+        output.innerHTML = `profit is ${profit} and percentage of profit is ${profitPercentage}`;
     }else{
-        console.log("no pain no gain and no gain no pain!")
+        output.innerHTML = "no pain no gain and no gain no pain!";
     }
 }
 
 function stockProfitLoss(){
-    calculateProfitLoss(10,10,10);
-    calculateProfitLoss(100, 10, 50);
-    calculateProfitLoss(100, 10, 200);
+    calculateProfitLoss( 
+        Number(initialValue.value), 
+        Number(quantityStock.value), 
+        Number(currentPrice.value)
+        );
 }
 
 tellMeButton.addEventListener('click', stockProfitLoss)
